d2c5f774cd2f0be1b16eea0c45141ed5e3727b21
Daniel Ben Moshe <daniel14bm@gmail.com>
2020-03-23 18:28:37 +0200

    Main block reconstructed
    premier isStay decision for each section

--- IsStay.m
+++ IsStay.m
@@ -20,6 +20,11 @@ function [ isStay ] = IsStay( accx, accy, accz, timestamp, params)
 %           max_time_gap_msec [dobule] - maximal time gap in miliseconds
 %                                        between foloowing timestamps for
 %                                        fs estimation
+%                      
+%           max_section_gap_minutes
+%                             [double] - maximal time gap between folloing
+%                                        samples, to be considered as same
+%                                        section
 %           
 %           ecdf_diff_th      [double] - possible update to var_th parameter
 %                                        by indicating a knee in the ECDF of
@@ -44,9 +49,12 @@ function [ isStay ] = IsStay( accx, accy, accz, timestamp, params)
 %       stay_durations  [Mx1 datetime] - sojourning durations if exist, empty otherwise
 %
 
-%% unpack 
+%% unpack & definitions
 MAX_HIST_BINS = 1e4;
+data_len = length(timestamp);
 var_th = params.var_th;
+acc_mat = [ accx , accy , accz ];
+Ndims = 3;
 
 %% calculate average sample rate
 %  filter outliers
@@ -54,8 +62,6 @@ var_th = params.var_th;
 
 time_diffs_msec = diff(datenum(timestamp)) * 24 * 60 * 60 * 1e3;  % (datestr(timestamp),'dd-mmm-yyyy hh:MM:ss'));
 fs = mean( 1e3 * 1 ./ time_diffs_msec( time_diffs_msec < params.max_time_gap_msec ));
-
-
 
 % purposed - data driven
 %   max_time_gap_msec_pctl [double] - pctile of maximal allowed time gap
@@ -70,7 +76,7 @@ abrupt_filt_len = sec2smp(params.abrupt_filt_time_const);
 
 %% optionally update var_th
 % vecnorm can be utilized starting from 2017b
-acc_abs = sqrt( accx.^2 + accy.^2 + accz.^2 );
+acc_abs = sqrt(sum( acc_mat.^2 ,2));
 acc_movevar = movvar(acc_abs, win_size_smp);
 [hist_counts, hist_centers] = hist(acc_movevar,linspace(min(acc_movevar),max(acc_movevar),MAX_HIST_BINS));
 
@@ -87,6 +93,29 @@ if ~isempty(knee_th)
    var_th = min( var_th , knee_th);
 end
 
+%% find all different sections
+section_idxs = find( time_diffs_msec > params.max_section_gap_minutes*60*1e3 );
 
+if isempty(section_idxs)
+    section_idxs = [1 data_len];
 end
 
+%% go through each section and decide isStay
+is_seperate_axis = [];  % should be realocated in other language
+is_abs_stay = [];       % should be realocated in other language
+for isect=1:size(section_idxs,1)
+    curr_section_idxs = section_idxs(isect,1) : section_idxs(isect,2)-1 ;
+    curr_acc_mat = acc_mat(section_idxs(curr_section_idxs,:));
+    curr_acc_abs = sqrt(sum(curr_acc_mat.^2,2)); 
+    mvr_mat = movvar(curr_acc_mat, win_size_smp, 0, 1);
+    mvr_abs = movvar(curr_acc_abs, win_size_smp);
+    
+    % check seperate axis
+    is_seperate_axis(curr_section_idxs) = all(mvr_mat < var_th/Ndims,2);
+    is_abs_stay(curr_section_idxs) = mvr_abs < var_th ;
+end
+isStay = is_seperate_axis & is_abs_stay ;
+
+%% filter abrupt movements
+
+end
